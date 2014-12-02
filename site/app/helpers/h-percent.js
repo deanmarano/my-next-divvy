4596d661eee697a541d9fcaab34e51aadb660712
Dean Marano <dmarano@groupon.com>
2014-12-02 11:37:21 -0600

    Increase displayed precision in math.

--- site/app/helpers/h-percent.js
+++ site/app/helpers/h-percent.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
-export function percent(input) {
-  return Math.round(input * 10000) / 100 + '%';
+export function percent(input, precision) {
+  if(typeof precision === 'number') {
+    return Math.round(input * (10000 * precision)) / (100 * precision) + '%';
+  } else {
+    return Math.round(input * 10000) / 100 + '%';
+  }
 }
 
 export default Ember.Handlebars.makeBoundHelper(percent);
